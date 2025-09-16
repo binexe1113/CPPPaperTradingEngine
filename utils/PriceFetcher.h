bd70f2ff0e9758dd7f472bfb00f398822bf477db
Fabio Henrique Lopes <fabiohblopes3@gmail.com>
2025-09-16 09:52:39 -0300

    feat: Introduce PricePoint struct and history storage in PriceFetcher

--- utils/PriceFetcher.h
+++ utils/PriceFetcher.h
@@ -1,12 +1,23 @@
 #pragma once
 #include <string>
 #include <vector>
+#include<deque>
 #include <unordered_map>
+#include <ctime>
+
+class PricePoint{
+public:
+    double price;
+    std::time_t timestamp;
+    PricePoint() = default;
+    PricePoint(double p, std::time_t t): price(p), timestamp(t){};
+};
 
 class PriceFetcher {
 private:
     std::vector<std::string> symbols;
     std::unordered_map<std::string, double> prices;
+    std::unordered_map<std::string, std::deque<PricePoint>> history;
 
 public:
     PriceFetcher(const std::vector<std::string>& symbols);
@@ -14,5 +25,7 @@ public:
     void updatePrices();  // updates existing prices
     double getPrice(const std::string& symbol); 
     std::string getRandomSymbol();
-    void printPrices() const; // optional, useful for debugging
+    void printPrices() const; // useful for debugging plus looks cool on CLI
+
+    const std::deque<PricePoint>& getHistory(const std:: string& symbol) const;
 };
